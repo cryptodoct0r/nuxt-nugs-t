031851a2feb367f744f8d9bcc9f5bea245b14b03
cryptodoctor <emmanuelneni@gmail.com>
2019-07-22 00:28:18 +0300

    removed unwanted routes

--- pages/User/signIn.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-container>
-    <h1 class="heading grey--text">SignIn Page</h1>
-  </v-container>
-</template>
--- pages/User/signUp.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-container>
-    <h1 class="heading grey--text">SignUp Page</h1>
-  </v-container>
-</template>
